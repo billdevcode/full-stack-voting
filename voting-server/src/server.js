cb503fbfedb42786856bcd14284a47fc06fb5dc2
Bill <billyd2013@gmail.com>
2016-06-22 03:48:51 +0000

    Finish server setup

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,6 +1,6 @@
 import Server from 'socket.io';
 
-export default function startServer(store) {
+export function startServer(store) {
     const io = new Server().attach(8090);
     
     store.subscribe( 
@@ -9,6 +9,8 @@ export default function startServer(store) {
     
     io.on('connection', (socket) => {
         socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
     });
+    
 }
 
